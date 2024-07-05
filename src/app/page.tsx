7a0edaeba2972e0e781b99de53cb769572f9062b
Aabhas Gurung <aabhas.gurung@vedastudios.com>
2024-07-05 11:39:10 +0545

    feat: created about us section in homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import { NavHeader } from '@/common/components/Molecules/NavHeader'
 import { HeroSection } from './(home)/_components/Hero/HeroSection'
+import AboutusWrapper from './(home)/_components/TeacherAndStaff/AboutusWrapper'
 
 export default function Home() {
   return (
     <>
       <NavHeader />
       <HeroSection />
+      <AboutusWrapper />
     </>
   )
 }
