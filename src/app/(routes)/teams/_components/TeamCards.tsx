e02db5850ba110f61eb6c7c7eb071e515ecf1798
Aabhas Gurung <aabhas.gurung@vedastudios.com>
2024-07-30 14:11:14 +0545

    feat: complete about and other pages

--- src/app/(routes)/teams/_components/TeamCards.tsx
+++ src/app/(routes)/teams/_components/TeamCards.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import React from 'react'
 import './teams.css'
 import { ITeamsData } from '../_interface/Teams'
+import Link from 'next/link'
 
 const TeamCards = ({ teams }: { teams: ITeamsData[] }) => {
   return (
@@ -34,21 +35,63 @@ const TeamCards = ({ teams }: { teams: ITeamsData[] }) => {
       <div className="hidden md:grid grid-cols-2 gap-16 lg:gap-[52px] lg:flex lg:flex-wrap justify-center items-center">
         {teams.map((team, index) => (
           <div
-            className="flex flex-col justify-center items-center"
+            className="flex flex-col justify-center items-center group"
             key={index}
           >
             <div
+              className="flex justify-center items-center border rounded-full w-[304px] lg:w-[236px] h-[304px] lg:h-[236px] border-primaryLight relative overflow-hidden"
               key={index}
-              className="flex justify-center items-center border rounded-full w-[304px] lg:w-[236px] h-[304px] lg:h-[236px] border-primaryLight relative"
             >
-              <Image
-                src={team.image}
-                width={236}
-                height={236}
-                alt="teams"
-                className="rounded-full w-[236px] lg:w-[200px] h-[236px] lg:h-[200px] object-cover border-primaryLight"
-              />
-              <div className="absolute inset-0 flex justify-center items-center">
+              <div className="relative  w-[236px] lg:w-[200px] h-[236px] lg:h-[200px]">
+                <Image
+                  src={team.image}
+                  width={236}
+                  height={236}
+                  alt="teams"
+                  className="rounded-full w-[236px] lg:w-[200px] h-[236px] lg:h-[200px] object-cover border-primaryLight transition duration-300 relative"
+                />
+                <div className="absolute inset-0 flex justify-center items-center transition-opacity duration-300 opacity-0 group-hover:opacity-100 bg-primary/[56%] rounded-full bg-opacity-70">
+                  <ul className="flex gap-4">
+                    <Link
+                      href="/"
+                      className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                    >
+                      <Image
+                        src="/home/ftwitter.svg"
+                        width={100}
+                        height={100}
+                        alt="social"
+                        className="w-4 h-4"
+                      />
+                    </Link>
+                    <Link
+                      href="/"
+                      className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                    >
+                      <Image
+                        src="/home/ffacebook.svg"
+                        width={100}
+                        height={100}
+                        alt="social"
+                        className="w-4 h-4"
+                      />
+                    </Link>
+                    <Link
+                      href="/"
+                      className="bg-white w-8 h-8 rounded-full flex justify-center items-center"
+                    >
+                      <Image
+                        src="/home/finstagram.svg"
+                        width={100}
+                        height={100}
+                        alt="social"
+                        className="w-4 h-4"
+                      />
+                    </Link>
+                  </ul>
+                </div>
+              </div>
+              <div className="absolute inset-0 flex justify-center items-center transition-transform duration-300 group-hover:rotate-[-25deg]">
                 <div className="diagonal-border"></div>
               </div>
             </div>
