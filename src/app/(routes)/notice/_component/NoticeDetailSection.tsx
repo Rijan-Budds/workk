367a927c42763fcffb18511810ccabb9f5221f3c
Ravi Neupane <ravi.neupane@vedastudios.com>
2024-09-10 11:43:14 +0545

    fix: notice detail ui

--- src/app/(routes)/notice/_component/NoticeDetailSection.tsx
+++ src/app/(routes)/notice/_component/NoticeDetailSection.tsx
@@ -6,17 +6,21 @@ import React from 'react'
 export const NoticeDetailSection = () => {
   return (
     <div className="flex flex-col items-start gap-y-2 md:max-w-[672px]  2lg:max-w-[787px]">
-      <MiniHeading>July 19, 2024</MiniHeading>
+      <MiniHeading className="font-semibold leading-4 tracking-widest text-secondary">
+        July 19, 2024
+      </MiniHeading>
       <SectionHeading>Admission opens for 2080</SectionHeading>
 
-      <p className="text-body font-workSans text-[16px] leading-[27.2px]">
+      <p className="text-body font-workSans text-[16px] leading-[27.2px] mt-8 ">
         We are more than glad to announce that the admissions are now open for
         academic session 2080 and we welcome young kids with arms wide open to
         push open the gates of our institution and grow, learn, flourish and
         reach sky high limits.
         <br />
         <br />
-        For more information, please contact : 015007275, 5007259
+        <span className="font-medium">
+          For more information, please contact : 015007275, 5007259
+        </span>
       </p>
 
       <Image
