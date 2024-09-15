634aec90d4d7688d9535a3e590442fd8dabc3ca7
Aabhas Gurung <aabhas.gurung@vedastudios.com>
2024-09-15 11:19:29 +0545

    fix: add api where it was missing for common components

--- src/app/(routes)/admission/AdmissionSection/AdmissionSection.tsx
+++ src/app/(routes)/admission/AdmissionSection/AdmissionSection.tsx
@@ -1,48 +1,72 @@
 'use client'
 import { HomeWrapper } from '@/common/components/Atom/HomeWrapper'
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 import AdmissionDetail from './AdmissionDetail'
 import AdmissionSideBar from './AdmissionSideBar'
 import { CoverImage } from '@/common/components/Molecules/CoverImage'
-import { ITeamsData } from '../../teams/_interface/Teams'
-import { TeamsData } from '../../teams/constants/data'
-import AdmissionFilter from '../_components/AdmissionFilter'
+// import { ITeamsData, ITeamsResponse } from '../../teams/_interface/Teams'
+// import AdmissionFilter from '../_components/AdmissionFilter'
+// import { UseServerFetch } from '@/common/hook/useServerFetch'
 
-type ITitle =
-  | 'Our Board Members'
-  | 'Our Expert Instructors'
-  | 'Administration Members'
+// type ITitle =
+//   | 'Our Board Members'
+//   | 'Our Expert Instructors'
+//   | 'Administration Members'
 
 const AdmissionSection = () => {
-  const [title, setTitle] = useState<ITitle>('Our Board Members')
-  const [filteredTeams, setFilteredTeams] = useState<ITeamsData[]>([]) // eslint-disable-line
+  // const [title, setTitle] = useState<ITitle>('Our Board Members')
+  // const [filteredTeams, setFilteredTeams] = useState<ITeamsData[]>([])
+  // const [response, setResponse] = useState<ITeamsResponse | null>(null)
 
-  useEffect(() => {
-    let filteredData: ITeamsData[] = []
-    if (title === 'Our Board Members') {
-      filteredData = TeamsData.filter((team) => team.type === 'board')
-    } else if (title === 'Our Expert Instructors') {
-      filteredData = TeamsData.filter((team) => team.type === 'instructor')
-    } else if (title === 'Administration Members') {
-      filteredData = TeamsData.filter((team) => team.type === 'administration')
-    }
-    setFilteredTeams(filteredData)
-  }, [title])
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response: ITeamsResponse | undefined = await UseServerFetch(
+  //         '/api/v1/team'
+  //       )
 
-  const handleDynamicData = (type: ITitle) => {
-    setTitle(type)
-  }
+  //       if (response) {
+  //         setResponse(response)
+  //       }
+  //     } catch (error) {
+  //       console.error('Error fetching data:', error)
+  //     }
+  //   }
+
+  //   fetchData()
+  // }, [])
+
+  // useEffect(() => {
+  //   if (response) {
+  //     const filteredData = response.data.filter((team: ITeamsData) => {
+  //       if (title === 'Our Board Members') {
+  //         return team.managementLevel === 'BOARD_MEMBER'
+  //       } else if (title === 'Our Expert Instructors') {
+  //         return team.managementLevel === 'EXPERT_INSTRUCTOR'
+  //       } else if (title === 'Administration Members') {
+  //         return team.managementLevel === 'ADMINISTRATION_MEMBER'
+  //       }
+  //       return false
+  //     })
+
+  //     setFilteredTeams(filteredData)
+  //   }
+  // }, [title, response])
+
+  // const handleDynamicData = (type: ITitle) => {
+  //   setTitle(type)
+  // }
   return (
     <div>
       <CoverImage
         title="Admission"
         list={[{ link: '', title: 'Admission' }]}
-        key={title}
+        // key={title}
       />
 
       <HomeWrapper>
         <div className="flex flex-col 2lg:flex-row gap-[80px] 2lg:gap-[56px]">
-          <AdmissionFilter handleDynamicData={handleDynamicData} />
+          {/* <AdmissionFilter handleDynamicData={handleDynamicData} /> */}
           <AdmissionDetail />
           <AdmissionSideBar />
         </div>
