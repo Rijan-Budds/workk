ff6fb08444dc6b21c30b2dd864157ad62c77e314
Ravi Neupane <ravi.neupane@vedastudios.com>
2024-09-16 15:26:13 +0545

    chore: resolve type for the notice section

--- src/app/(routes)/notice/_component/NoticeSection.tsx
+++ src/app/(routes)/notice/_component/NoticeSection.tsx
@@ -1,15 +1,14 @@
 import { HomeWrapper } from '@/common/components/Atom/HomeWrapper'
 import { CoverImage } from '@/common/components/Molecules/CoverImage'
 import React from 'react'
-import { NoticeClientSection } from './NoticeClientSection'
 
 export const NoticeSection = () => {
   return (
     <>
       <CoverImage title="Notices" list={[{ link: '', title: 'Notices' }]} />
       <HomeWrapper>
-        <div className="flex flex-col  gap-y-10 2lg:items-center">
-          <NoticeClientSection />
+        <div className="flex flex-col  gap-y-10 2lg:items-center bdr">
+          {/* <NoticeClientSection /> */}
         </div>
       </HomeWrapper>
     </>
