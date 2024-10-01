d6b49e6911156a2c215eb7217367862136cb6c49
Sandip Nepali <sandip.nepali@vedastudio.com>
2024-10-01 11:27:12 +0545

    fix: adjust size of image and disable next and prev button

--- src/app/(routes)/academics/school/_components/SchoolGallery.tsx
+++ src/app/(routes)/academics/school/_components/SchoolGallery.tsx
@@ -1,16 +1,17 @@
 'use client'
-import React, { useEffect, useState } from 'react'
-import { Swiper, SwiperSlide } from 'swiper/react'
-import 'swiper/css'
-import Image from 'next/image'
 import {
   SwiperButtonNext,
   SwiperButtonPrevious,
 } from '@/common/components/Atom/SwiperButton'
-import { IoIosArrowRoundBack, IoIosArrowRoundForward } from 'react-icons/io'
 import { Button } from '@/common/components/ui/button'
-import Link from 'next/link'
 import { UseServerFetch } from '@/common/hook/useServerFetch'
+import Image from 'next/image'
+import Link from 'next/link'
+import React, { useEffect, useState } from 'react'
+import { IoIosArrowRoundBack, IoIosArrowRoundForward } from 'react-icons/io'
+import { MdOutlineChevronRight } from 'react-icons/md'
+import 'swiper/css'
+import { Swiper, SwiperSlide } from 'swiper/react'
 import {
   ISchoolGallery,
   ISchoolGalleryResponse,
@@ -69,9 +70,14 @@ const SchoolGallery = () => {
           <div className="mt-6">
             <Button
               variant="secondary"
-              className="w-full font-workSans text-heading text-lg font-medium"
+              className="group w-full flex items-center gap-4 font-workSans text-heading text-lg font-medium transition-all duration-500"
             >
               View Our Gallery
+              <MdOutlineChevronRight
+                width={24}
+                height={24}
+                className="group-hover:rotate-90 transition-all duration-500"
+              />
             </Button>
           </div>
         </Link>
@@ -89,8 +95,39 @@ const ThumbnailSwiper = ({
   setActiveSrc: (src: string) => void
   galleries: ISchoolGallery[]
 }) => {
+  const [isBeginning, setIsBeginning] = useState(true)
+  const [isEnd, setIsEnd] = useState(false)
+  useEffect(() => {
+    setIsEnd(false)
+  }, [])
   return (
-    <Swiper spaceBetween={10} slidesPerView={4} className="relative">
+    <Swiper
+      breakpoints={{
+        360: {
+          spaceBetween: 0,
+          slidesPerView: 3.5,
+        },
+        400: {
+          spaceBetween: 0,
+          slidesPerView: 4,
+        },
+        736: {
+          spaceBetween: 4,
+          slidesPerView: 4.5,
+        },
+        1280: {
+          spaceBetween: 0,
+          slidesPerView: 4.5,
+        },
+      }}
+      onReachBeginning={() => setIsBeginning(true)}
+      onReachEnd={() => setIsEnd(true)}
+      onFromEdge={() => {
+        setIsBeginning(false)
+        setIsEnd(false)
+      }}
+      className="relative"
+    >
       {galleries.map((gallery, index) => (
         <SwiperSlide key={index}>
           <Image
@@ -102,7 +139,7 @@ const ThumbnailSwiper = ({
             src={`${process.env.NEXT_PUBLIC_IMAGE_URL}/${gallery.photo?.key}`}
             width={787}
             height={524}
-            className={`rounded-xl cursor-pointer h-[85px] md:h-[152px] object-cover ${
+            className={`rounded-xl cursor-pointer w-[85px] h-[85px] md:h-[152px] md:w-[152px] object-cover ${
               activeSrc ===
               `${process.env.NEXT_PUBLIC_IMAGE_URL}/${gallery.photo?.key}`
                 ? 'border-2 border-primary'
@@ -115,12 +152,20 @@ const ThumbnailSwiper = ({
 
       <div className="absolute right-[3px] top-1/2 -translate-y-1/2 z-10">
         <SwiperButtonNext>
-          <IoIosArrowRoundForward className="text-body text-2xl font-light bg-white rounded-full size-7" />
+          <IoIosArrowRoundForward
+            className={`text-body text-2xl font-light bg-white rounded-full size-7 ${
+              isEnd ? 'opacity-25 cursor-not-allowed' : ''
+            }`}
+          />
         </SwiperButtonNext>
       </div>
       <div className="absolute left-0 top-1/2 -translate-y-1/2 z-10">
         <SwiperButtonPrevious>
-          <IoIosArrowRoundBack className="text-body text-2xl font-light bg-white rounded-full size-7" />
+          <IoIosArrowRoundBack
+            className={`text-body text-2xl font-light bg-white rounded-full size-7 ${
+              isBeginning ? 'opacity-25 cursor-not-allowed' : ''
+            }`}
+          />
         </SwiperButtonPrevious>
       </div>
     </Swiper>
