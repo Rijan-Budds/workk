fceb9ef4b99c51e683bafb794ffd4bd0884d92e1
Ravi Neupane <ravi.neupane@vedastudios.com>
2024-10-03 10:59:41 +0545

    fix: trigger dialog box even when no field is changed

--- src/app/(routes)/apply/page.tsx
+++ src/app/(routes)/apply/page.tsx
@@ -1,21 +1,7 @@
-'use client'
-
-import React, { useEffect } from 'react'
+import React from 'react'
 import { Admission } from './_component/Admission'
 
 const ApplyPage = () => {
-  useEffect(() => {
-    const handleBeforeReload = (event: BeforeUnloadEvent) => {
-      event.preventDefault()
-      return ''
-    }
-
-    window.addEventListener('beforeunload', handleBeforeReload)
-
-    return () => {
-      window.removeEventListener('beforeunload', handleBeforeReload)
-    }
-  }, [])
   return <Admission />
 }
 
