2eaa7042ba00154e56828bf5f92740d829e5bec6
Aabhas Gurung <aabhas.gurung@vedastudios.com>
2024-10-07 11:46:25 +0545

    feat: pull dev solve conflicts and push

--- src/app/(routes)/news/_component/MobileSidebar.tsx
+++ src/app/(routes)/news/_component/MobileSidebar.tsx
@@ -1,18 +1,58 @@
-import React from 'react'
+'use client'
+import React, { useEffect, useState } from 'react'
 import { DownloadUi, RecentPostUi } from './DownloadUi'
 import { SidebarAd } from './SidebarAd'
 import { INewsResponseData } from '../interface/newsType'
 import { UseServerFetch } from '@/common/hook/useServerFetch'
+import {
+  IBrochureApiResponse,
+  IBrochureItem,
+} from '../../brochure/interface/brochureType'
 
-export const MobileSidebar = async ({ type }: { type: 'NEWS' | 'NOTICE' }) => {
-  const response: INewsResponseData | undefined = await UseServerFetch(
-    `/api/v1/news-and-notice/type/${type}`
+export const MobileSidebar = ({ type }: { type: 'NEWS' | 'NOTICE' }) => {
+  const [brochureData, setBrochureData] = useState<IBrochureItem[] | undefined>(
+    undefined
   )
+
+  const [response, setResponse] = useState<INewsResponseData | null>(null)
+
+  useEffect(() => {
+    const fetchResponse = async () => {
+      try {
+        const response: INewsResponseData | undefined = await UseServerFetch(
+          `/api/v1/news-and-notice/type/${type}`
+        )
+        if (response) {
+          setResponse(response)
+        }
+      } catch (error) {
+        console.error('Error fetching brochure data:', error)
+      }
+    }
+
+    fetchResponse()
+  }, [])
+
+  useEffect(() => {
+    const fetchBrochureResponse = async () => {
+      try {
+        const brochureDataResponse: IBrochureApiResponse | undefined =
+          await UseServerFetch(`/api/v1/brochure`)
+        if (brochureDataResponse) {
+          setBrochureData(brochureDataResponse?.data)
+        }
+      } catch (error) {
+        console.error('Error fetching brochure data:', error)
+      }
+    }
+
+    fetchBrochureResponse()
+  }, [])
 
   return (
     <div className="flex flex-col 2lg:hidden gap-y-6">
       <RecentPostUi recentData={response?.data ? response?.data : undefined} />
-      <DownloadUi />
+      {brochureData && <DownloadUi data={brochureData} />}
       <SidebarAd />
     </div>
   )
--- src/app/(routes)/news/_component/StickySidebar.tsx
+++ src/app/(routes)/news/_component/StickySidebar.tsx
@@ -1,23 +1,64 @@
+'use client'
 import { UseServerFetch } from '@/common/hook/useServerFetch'
 import { INewsData, INewsResponseData } from '../interface/newsType'
-import { RecentPostUi } from './DownloadUi'
+import { DownloadUi, RecentPostUi } from './DownloadUi'
 import { SidebarAd } from './SidebarAd'
+import { useEffect, useState } from 'react'
+import {
+  IBrochureApiResponse,
+  IBrochureItem,
+} from '../../brochure/interface/brochureType'
 
-export const StickySidebar = async ({
+export const StickySidebar = ({
   type,
   data,
 }: {
   type: 'NEWS' | 'NOTICE'
   data?: INewsData
 }) => {
-  const response: INewsResponseData | undefined = await UseServerFetch(
-    `/api/v1/news-and-notice/type/${type}`
+  const [brochureData, setBrochureData] = useState<IBrochureItem[] | undefined>(
+    undefined
   )
+
+  const [response, setResponse] = useState<INewsResponseData | null>(null)
+
+  useEffect(() => {
+    const fetchResponse = async () => {
+      try {
+        const response: INewsResponseData | undefined = await UseServerFetch(
+          `/api/v1/news-and-notice/type/${type}`
+        )
+        if (response) {
+          setResponse(response)
+        }
+      } catch (error) {
+        console.error('Error fetching brochure data:', error)
+      }
+    }
+
+    fetchResponse()
+  }, [])
+
+  useEffect(() => {
+    const fetchBrochureResponse = async () => {
+      try {
+        const brochureDataResponse: IBrochureApiResponse | undefined =
+          await UseServerFetch(`/api/v1/brochure`)
+        if (brochureDataResponse) {
+          setBrochureData(brochureDataResponse?.data)
+        }
+      } catch (error) {
+        console.error('Error fetching brochure data:', error)
+      }
+    }
+
+    fetchBrochureResponse()
+  }, [])
 
   return (
     <div className="max-h-[1015px] w-[397px] bg-white  sticky top-24 transition-all duration-700  flex-col gap-y-6 hidden 2lg:flex  ">
       <RecentPostUi recentData={response?.data ? response?.data : undefined} />
-      {/* <DownloadUi /> */}
+      {brochureData && <DownloadUi data={brochureData} />}
       {data && <SidebarAd data={data} />}
     </div>
   )
