034151133996114a7cfa851149e2fdd3ee0c1190
Sandip Nepali <sandip.nepali@vedastudio.com>
2024-10-07 09:26:16 +0545

    fix: breadcrumbs links of terms and conditions

--- src/app/(routes)/terms-and-condition/_components/TearmsAndCondition.tsx
+++ src/app/(routes)/terms-and-condition/_components/TearmsAndCondition.tsx
@@ -1,17 +1,15 @@
-import { UseServerFetch } from '@/common/hook/useServerFetch'
-import React from 'react'
-import { ITermsResponse } from '../_interface/Terms'
 import { HomeWrapper } from '@/common/components/Atom/HomeWrapper'
 import { CoverImage } from '@/common/components/Molecules/CoverImage'
 import { NoDataFound } from '@/common/components/NoDataFound'
+import { UseServerFetch } from '@/common/hook/useServerFetch'
 import { format } from 'date-fns'
+import { ITermsResponse } from '../_interface/Terms'
 import './terms.css'
 
 const TearmsAndCondition = async () => {
   const response: ITermsResponse | undefined = await UseServerFetch(
     '/api/v1/terms-and-conditions'
   )
-  console.log(response?.data[0]?.createdAt)
 
   const renderTermsUi = () => {
     if (response?.data && response.data.length > 0) {
@@ -43,7 +41,7 @@ const TearmsAndCondition = async () => {
     <div>
       <CoverImage
         title="Terms and Conditions"
-        list={[{ link: 'about', title: 'Terms and Condition' }]}
+        list={[{ link: 'terms-and-condition', title: 'Terms and Condition' }]}
       />
       <HomeWrapper className="!px-[551px]">{renderTermsUi()}</HomeWrapper>
     </div>
