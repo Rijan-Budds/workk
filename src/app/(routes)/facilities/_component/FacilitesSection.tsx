c2cf066592ea2bf0d6fc960e53102ad6d43cef47
Rijan-Budds <buddsrijan@gmail.com>
2025-08-04 08:00:17 +0545

    another try to deploy to netlify

--- src/app/(routes)/facilities/_component/FacilitesSection.tsx
+++ src/app/(routes)/facilities/_component/FacilitesSection.tsx
@@ -3,13 +3,29 @@ import { CoverImage } from '@/common/components/Molecules/CoverImage'
 import React from 'react'
 import { FacilitesTabSection } from './FacilitesTabSection'
 
+// Allowed slugs
+const validSlugs = ['science-lab', 'library', 'transportation', 'school-canteen'] as const
+type FacilitySlug = typeof validSlugs[number]
+
+function isValidFacilitySlug(slug: string): slug is FacilitySlug {
+  return validSlugs.includes(slug as FacilitySlug)
+}
+
 export const FacilitesSection = ({ slug }: { slug: string }) => {
+  if (!isValidFacilitySlug(slug)) {
+    return (
+      <div className="min-h-screen flex items-center justify-center">
+        <p className="text-red-500 text-lg">Invalid facility</p>
+      </div>
+    )
+  }
+
   return (
-    <div className="min-h-screen ">
+    <div className="min-h-screen">
       <CoverImage
         title={slug}
         list={[
-          { link: '/facilities', title: 'facilites' },
+          { link: '/facilities', title: 'facilities' },
           { link: null, title: slug },
         ]}
       />
